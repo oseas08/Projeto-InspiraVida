650a778deb4f48a1afc7bb90e023ca94ed4dfdf9
Rubia15 <rbl3@aluno.ifal.edu.br>
2025-06-10 16:59:54 -0300

    Adicionei a foto para o prfil e as inforamções superiores. Está quase concluída, faltando apenasu m button.

--- lib/perfil.dart
+++ lib/perfil.dart
@@ -13,24 +13,37 @@ class _PerfilState extends State<Perfil> {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          backgroundColor: Color(0xFF49d3ee) ,
+          backgroundColor: Color(0xFF49d3ee),
           toolbarHeight: 300,
-          flexibleSpace: Padding(padding: EdgeInsets.all(8),
+          flexibleSpace: Padding(
+            padding: EdgeInsets.all(8),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
+
               children: [
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    IconButton(onPressed: (){}, icon: Icon(Icons.arrow_back_ios)),
+                    IconButton(
+                        onPressed: () {}, icon: Icon(Icons.arrow_back_ios)),
                     Text(
                       "Perfil",
                     )
-                  ],),
-
-
+                  ],
+                ),
+                Column(
+                  children: [
+                    Image.asset("assets/personagem.projeto-semfundo.png", height: 150,),
+                    Text("Jack Willson", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
+                    SizedBox(height: 10,),
+                    Text("jackwillson123@gmail.com", style: TextStyle(
+                        color: Color(0xFF494949),)
+                    )
+                  ],
+                )
               ],
-            ),),),
+            ),
+          ),
+        ),
         body: Container(
           padding: EdgeInsets.all(24),
           color: Color(0xFF0097b2),
@@ -43,23 +56,21 @@ class _PerfilState extends State<Perfil> {
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-
                       Row(
                         children: [
                           SizedBox(width: 10),
                           Icon(Icons.person, size: 32),
                           SizedBox(width: 10),
-                          Text("Jack Willson", style: TextStyle(
-                            fontSize: 24,
-                          )),
+                          Text("Jack Willson",
+                              style: TextStyle(
+                                fontSize: 24,
+                              )),
                         ],
                       ),
-                      Row(
-                          children: [
-                            Icon(Icons.edit, size: 32),
-                            SizedBox(width: 10)
-                          ]
-                      ),
+                      Row(children: [
+                        Icon(Icons.edit, size: 32),
+                        SizedBox(width: 10)
+                      ]),
                     ],
                   ),
                 ),
@@ -69,80 +80,74 @@ class _PerfilState extends State<Perfil> {
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-
                       Row(
                         children: [
                           SizedBox(width: 10),
                           Icon(Icons.email, size: 32),
                           SizedBox(width: 10),
-                          Text("jackwillson123@gmail.com", style: TextStyle(
-                            fontSize: 22,
-                          )),
+                          Text("jackwillson123@gmail.com",
+                              style: TextStyle(
+                                fontSize: 22,
+                              )),
                         ],
                       ),
-                      Row(
-                          children: [
-                            Icon(Icons.edit, size: 32),
-                            SizedBox(width: 10)
-                          ]
-                      )
+                      Row(children: [
+                        Icon(Icons.edit, size: 32),
+                        SizedBox(width: 10)
+                      ])
                     ],
-                  ),),
-
+                  ),
+                ),
                 Container(
                   height: 50,
                   color: Colors.white,
                   child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-
                         Row(
                           children: [
                             SizedBox(width: 10),
                             Icon(Icons.lock_rounded, size: 32),
                             SizedBox(width: 10),
-                            Text("Senha antiga", style: TextStyle(
-                              fontSize: 22,
-                            )),
+                            Text("Senha antiga",
+                                style: TextStyle(
+                                  fontSize: 22,
+                                )),
                           ],
                         ),
-                        Row(
-                            children: [
-                              Icon(Icons.edit, size: 32),
-                              SizedBox(width: 10)
-                            ]
-                        ),
-                      ] ),),
+                        Row(children: [
+                          Icon(Icons.edit, size: 32),
+                          SizedBox(width: 10)
+                        ]),
+                      ]),
+                ),
                 Container(
                   height: 50,
                   color: Colors.white,
                   child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-
                         Row(
                           children: [
                             SizedBox(width: 10),
                             Icon(Icons.lock, size: 32),
                             SizedBox(width: 10),
-                            Text("Nova senha", style: TextStyle(
-                              fontSize: 22,
-                            )),
+                            Text("Nova senha",
+                                style: TextStyle(
+                                  fontSize: 22,
+                                )),
                           ],
                         ),
-                        Row(
-                            children: [
-                              Icon(Icons.edit, size: 32),
-                              SizedBox(width: 10)
-                            ]
-                        ),
-                      ]
-
-                  ),
+                        Row(children: [
+                          Icon(Icons.edit, size: 32),
+                          SizedBox(width: 10)
+                        ]),
+                      ]),
                 ),
-                SizedBox(height: 200,)
-              ]
-          ),
+                SizedBox(
+                  height: 200,
+                )
+              ]),
         ),
       ),
     );
